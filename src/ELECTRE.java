36f20a3656848c44ccf4514ed22b388f5ac80dd9
mathntanzi0 <108949249+mathntanzi0@users.noreply.github.com>
2024-05-14 11:46:59 +0200

    ELECTRE layout

---
+++ src/ELECTRE.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class ELECTRE {
+    
+    public ArrayList<ArrayList<Integer>> getConcordanceSets(){
+        return null;
+    }
+    public ArrayList<ArrayList<Integer>> getDiscordanceSets(){
+        return null;
+    }
+    public double[][] concordanceMatrix(){
+        return null;
+    }
+    public double[][] discordanceMatrix(){
+        return null;
+    }
+    public double[][] concordanceDominanceMatrix(){
+        return null;
+    }
+    public double[][] discordanceDominanceMatrix(){
+        return null;
+    }
+    public double[][] aggregateDominanceMatrix(){
+        return null;
+    }
+    public void rankAlternatives(){
+        
+    }
+}
